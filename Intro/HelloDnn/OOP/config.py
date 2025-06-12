cc6fce73a4304c00b35746f688f6d7e835686cf0
3yassu <68251430+3yassu@users.noreply.github.com>
2025-06-11 22:57:21 -0500

    Delete Intro/HelloDnn/OOP directory

--- Intro/HelloDnn/OOP/config.py
+++
@@ -1,4 +0,0 @@
-#config.py
-class Config:
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
--- Intro/HelloDnn/OOP/driver.py
+++
@@ -1,8 +0,0 @@
-#driver.py
-from executive import Executive
-
-def main():
-	runner = Executive()
-	runner.train(0)
-	runner.print()
-main()
